2ac714358842343ce7399a49a817ccfc9d009e22
Fela Winkelmolen <fela.kde@gmail.com>
2010-05-23 09:35:22 +0200

    Initial code for brackets matching

--- h-ety-h/markup.rb
+++ h-ety-h/markup.rb
@@ -27,6 +27,7 @@ module HH::Markup
   def highlight str, colors = COLORS
     ary = []
     TOKENIZER.tokenize(str) do |token|
+      puts "'#{token}' #{token.group}/#{token.instruction}"
       ary <<
         if colors[token.group]
           span(token, colors[token.group])
@@ -40,5 +41,91 @@ module HH::Markup
     # puts "---"
     ary
   end
+
+  def matching_token(str, pos)
+    curr_pos = 0;
+    tokens = []
+    token_index = nil
+    start1 = nil
+    end1 = nil
+    start2 = nil
+    end2 = nil
+    TOKENIZER.tokenize(str) do |token|
+      curr_pos += token.size
+      if curr_pos >= curr_pos and token_index.nil?
+        token_index = tokens.size
+        start1 = curr_pos
+        end1 = curr_pos + token.size
+      end
+      tokens << token
+    end
+    if token_index.nil? then return nil end
+
+    if BRACKATS[token]
+      pos, len = matching_bracket(tokens, token_index, start1)
+    end
+
+    if pos
+      start2 = pos
+      end2 = pos + len
+    end
+
+    [start1, end1, start2, end2]
+  end
+
+  def matching_bracket(tokens, index, pos)
+    token = tokens['index']
+    if (matching = OPEN_BRACKETS[token])
+      direction = 1
+    elsif (matching = CLOSE_BRACKETS[token])
+      direction = -1
+    else
+      # something strange happened..
+      raise "internal error: unknown bracket"
+    end
+
+    stack_level = 1
+    while index >= 0 and index < tokens.size
+      index += direction
+      t = tokens[index]
+      pos += t.size * direction
+      if t == matching
+        stack_level -= 1
+        return [pos, t.size] if stack_level == 0
+      elsif t == token
+        stack_level += 1
+      end
+    end
+    return nil
+  end
+
+  OPEN_BRACKETS = {
+    '{' => '}',
+    '(' => ')',
+    '[' => ']',
+  }
+
+  #close_bracket = {}
+  #OPEN_BRACKETS.each{|open, close| opens_bracket[close] = open}
+  #CLOSE_BRACKETS = opens_bracket
+  # the following is more readable :)
+  CLOSE_BRACKETS = {
+    '}' => '{',
+    ')' => '(',
+    ']' => '[',
+  }
+
+  BRACKETS = CLOSE_BRACKETS.keys + OPEN_BRACKETS.keys
+
+#  MATCH_CLOSES = {
+#    ['do', :keyword] => ['end'],
+#    ['class', :keyword] => ['end'],
+#    ['module', :keyword] => ['end'],
+#    ['def', :keyword] => ['end'],
+#    ['if', :keyword] => ['end'],
+#    ['{', :punct] => ['}'],
+#    ['[', :punct] => [']'],
+#    ['(', :punct] => [')']
+#  }
 
 end
