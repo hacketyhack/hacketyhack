705adf869881918e49d5be920290a66611412b09
Fela Winkelmolen <fela.kde@gmail.com>
2010-06-12 13:51:23 +0200

    refactoring of the turtle code
    I've made the code a little more elegant. Now there is a
    Shoes::Turtle class, so more turtles can be run at the same time.

--- h-ety-h/turtlescript.rb
+++ h-ety-h/turtlescript.rb
@@ -1,97 +1,98 @@
-class Shoes
-  def add_turtle_methods args={}
-    @width = args[:width] || 400
-    @height = args[:height] || 400
+class Shoes::Turtle < Shoes::Widget
+  WIDTH = 400
+  HEIGHT = 400
+  include Math
+  DEG = PI / 180.0
 
-    def self.set_defaults
-      @heading = 180*Turtle::DEG
-      @x = @width/2
-      @y = @height/2
-      @pen_down = true
-    end
-
-    def self.reset
-      set_defaults
-      clear
-    end
-    def self.forward len=100
-      x = len*Math.sin(@heading) + @x
-      y = len*Math.cos(@heading) + @y
-      if @pen_down
-        l = [@x, @y, x, y]
-        line(*l)
-      end
-      @x, @y = x, y
-    end
-    # adds singleton methods
-    def self.backward len=100
-      forward(-len)
-    end
-    def self.turnleft angle=90
-      @heading += angle*Turtle::DEG
-      @heading %= 2*PI
-    end
-    def self.turnright angle=90
-      turnleft(-angle)
-    end
-    def self.setheading dir=180
-      dir += 180
-      @heading = dir*Turtle::DEG
-    end
-    def self.penup
-      @pen_down = false
-    end
-    def self.pendown
-      @pen_down = true
-    end
-    def self.isdown?
-      return @pen_down
-    end
-    def self.go x, y
-      @x, @y = x, y
-    end
-    def self.center
-      go(200, 200)
-    end
-    def self.setx x
-      @x = x
-    end
-    def self.sety y
-      @y = y
-    end
-    def self.getx
-      @x
-    end
-    def self.gety
-      @y
-    end
-    def self.getposition
-      [@x, @y]
-    end
-
-    def self.getheading
-      @heading
-    end
-
-    alias pencolor stroke
-    alias pensize strokewidth
-
-    # already in shoes:
-    # clear
-    # background
-
-    set_defaults
+  def initialize
+    @width = WIDTH
+    @height = WIDTH
+    @heading = 180*DEG
+    @x = @width / 2
+    @y = @height / 2
+    @pendown = true
   end
+
+  def reset
+    set_defaults
+    clear
+  end
+  def forward len=100
+    x = len*sin(@heading) + @x
+    y = len*cos(@heading) + @y
+    if @pendown
+      l = [@x, @y, x, y]
+      line(*l)
+    end
+    @x, @y = x, y
+  end
+  # adds singleton methods
+  def backward len=100
+    forward(-len)
+  end
+  def turnleft angle=90
+    @heading += angle*DEG
+    @heading %= 2*PI
+  end
+  def turnright angle=90
+    turnleft(-angle)
+  end
+  def setheading direction=180
+    direction += 180
+    direction %= 360
+    @heading = direction*DEG
+  end
+  def penup
+    @pendown = false
+  end
+  def pendown
+    @pendown = true
+  end
+  def isdown?
+    return @pendown
+  end
+  def go x, y
+    @x, @y = x, y
+  end
+  def center
+    go(200, 200)
+  end
+  def setx x
+    @x = x
+  end
+  def sety y
+    @y = y
+  end
+  def getx
+    @x
+  end
+  def gety
+    @y
+  end
+  def getposition
+    [@x, @y]
+  end
+
+  def getheading
+    @heading
+  end
+
+  alias pencolor stroke
+  alias pensize strokewidth
+
+  # already in shoes:
+  # clear
+  # background
 end
 
-
 module Turtle
-  DEG = Math::PI / 180.0
-  def self.start &blk
-    Shoes.app :width => 400, :height => 400 do
-      add_turtle_methods
-      instance_eval &blk
+  def self.start opts={}, &blk
+    opts[:width] ||= Shoes::Turtle::WIDTH
+    opts[:height] ||= Shoes::Turtle::HEIGHT
+    Shoes.app opts do
+      turtle.instance_eval &blk
     end
+
   end
 end
 
