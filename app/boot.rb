3fd3e9c9c6405039e802795ff0bedffff35fed47
Steve Klabnik <steve@steveklabnik.com>
2010-12-18 22:51:22 -0500

    Everyone starts with Hello World

--- app/boot.rb
+++ app/boot.rb
@@ -22,3 +22,12 @@ require 'app/ui/lessons'
 require 'app/ui/widgets'
 require 'app/ui/completion'
 require 'app/ui/tabs/sidetabs'
+
+#let's give them a simple program to start off with!
+if HH::PREFS['first_run'].nil?
+  File.open(File.join(HH::USER, "Hello World.rb"), "w") do |f|
+    f << 'alert "Hello, world!"'
+  end 
+
+  #the first_run pref will get set by the tour notice in app/ui/mainwindow
+end
