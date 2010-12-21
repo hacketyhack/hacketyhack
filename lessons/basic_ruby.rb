fad1ec1ab2e9584122df6ce6999648b1108914b2
Steve Klabnik <steve@steveklabnik.com>
2010-12-20 23:31:10 -0500

    Turtle lesson layout, renaming old lesson.
    I've got a lot of writing to do. *cracks knuckles*

---
+++ lessons/basic_ruby.rb
@@ -0,0 +1,39 @@
+lesson_set "Beginning programming" do
+
+  lesson "Hello there!"
+  page "Temp" do
+  end
+
+  lesson "Displaying Things"
+  page "Temp" do
+  end
+
+  lesson "Numbers and Math"
+  page "Temp" do
+  end
+
+  lesson "Letters, words, and sentences"
+  page "Temp" do
+  end
+
+  lesson "Variables"
+  page "Temp" do
+  end
+
+  lesson "User Input"
+  page "Temp" do
+  end
+
+  lesson "Basic flow control"
+  page "Temp" do
+  end
+
+  lesson "Example: a guessing game"
+  page "Temp" do
+  end
+
+  lesson "Summary"
+  page "Temp" do
+  end
+
+end
--- lessons/beginning_programming.rb
+++ lessons/beginning_programming.rb
@@ -1,39 +1,95 @@
 lesson_set "Beginning programming" do
 
   lesson "Hello there!"
-  page "Temp" do
+  page "Round One" do
   end
 
-  lesson "Displaying Things"
-  page "Temp" do
+  page "Lesson Controls" do
   end
 
-  lesson "Numbers and Math"
-  page "Temp" do
+  lesson "Let's talk about programming"
+  page "It's all about instructions" do
   end
 
-  lesson "Letters, words, and sentences"
-  page "Temp" do
+  page "The computer is simple" do
   end
 
-  lesson "Variables"
-  page "Temp" do
+  page "Explain yourself well" do
   end
 
-  lesson "User Input"
-  page "Temp" do
+  lesson "Lists of Instructions"
+  page "A to-do list, not a shoping list" do
   end
 
-  lesson "Basic flow control"
-  page "Temp" do
+  page "How would you tell a person to do it?" do
   end
 
-  lesson "Example: a guessing game"
-  page "Temp" do
+  page "Once again: computers are simple" do
+  end
+
+  lesson "Turtles, all the way down."
+  page "Drawing... with turtles?" do
+  end
+
+  page "The Turtle.draw command" do
+  end
+
+  page "Do... what I tell you to" do
+  end
+
+  page "Keeping it clean" do
+  end
+
+  page "Changing the background" do
+  end
+
+  page "The pen" do
+  end
+
+  page "Changing the pen color" do
+  end
+
+  page "Turtle coordinates." do
+  end
+
+  lesson "Drawing lines"
+  page "Sally forth!" do
+  end
+
+  page "Run away, Run away!" do
+  end
+
+  page "You spin me right round, baby" do
+  end
+
+  page "I like to move it, move it" do
+  end
+
+  page "Enough bad puns!" do
+  end
+
+  lesson "Repeating ourselves"
+  page "Remember those lists of instructions?" do
+  end
+
+  page "Pete and Repeat..." do
+  end
+
+  page "do... until the end" do
+  end
+
+  page "5.times" do
+  end
+
+  lesson "A little bit of random"
+  page "... never hurt anybody" do
+  end
+
+  page "Randomness is fun!" do
   end
 
   lesson "Summary"
-  page "Temp" do
+  page "Congradulations!" do
   end
 
 end
